1fab7e4a1c3f6a88eb3680012864f2e46b557c4d
中村勇斗 <82431426+yutwins@users.noreply.github.com>
2022-05-30 06:39:55 +0900

    add tasks

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([
     "ああああ",
     "いいいいいい"
   ]);
   const [completeTodos, setCompleteTodos] = useState(["うううううううう"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
