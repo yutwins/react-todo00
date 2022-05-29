aea861c0f21fa4a26855b55a9ed42f32f6a560bd
中村勇斗 <82431426+yutwins@users.noreply.github.com>
2022-05-30 06:27:58 +0900

    053000

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-export const App = () => {
-  return <div></div>;
-};
---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["うううううううう"]);
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
